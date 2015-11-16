809a1b9104cd6ba6eba3ba36d5308385d45d2fab
Diana Ortega <dicaormu@yahoo.com>
2015-11-16 09:29:22 +0100

    adding log

--- src/main/java/fr/com/cb/Match.java
+++ src/main/java/fr/com/cb/Match.java
@@ -2,22 +2,25 @@ package fr.com.cb;
 
 
 import fr.com.strategy.CustomProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Match {
 
+    private final Logger logger = LoggerFactory.getLogger(Match.class);
     private Game game;
-
 
     public String play(String number, CustomProcessor<String> moves) {
         game = Game.build(number).orElseThrow(IllegalStateException::new);
         String line, matchFactor = CustomProcessor.FINALIZER;
         while (!isWin(matchFactor, number.length()) && (line = moves.nextLine()) != CustomProcessor.FINALIZER) {
             matchFactor = game.guessNumber(line);
+            logger.debug("Getting  {} for number {} ", matchFactor, line);
         }
         return matchFactor;
     }
 
-    public boolean isWin(String response, int len) {
+    private boolean isWin(String response, int len) {
         String regex = "\\+*";
         return response.matches(regex) && response.length() == len;
     }
